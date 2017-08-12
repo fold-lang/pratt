42e5afe7a9746e412ab2864801c0e692cb855331
Rizo Isrof <rizo@odis.io>
2017-08-12 13:17:45 +0100

    Partial stream

--- src/Stream.ml
+++ src/Stream.ml
@@ -1,76 +1,68 @@
 
-type 'a t = unit -> 'a s
-and 'a s = Yield of 'a * 'a t | Empty
+type 'a t = Yield of 'a * (unit -> 'a t) | Empty
 
-let rec count n () = Yield (n, count (n + 1))
+let rec count n : int t = Yield (n, fun () -> count (n + 1))
 
-let rec make n f =
-  let rec loop i () =
+let rec make n f : 'a t =
+  let rec loop i =
     if i = n then Empty
-    else Yield (f i, loop (i + 1)) in
+    else Yield (f i, fun () -> loop (i + 1)) in
   loop 0
 
-let rec fold f acc stream =
-  match stream () with
+let rec fold f acc (stream : 'a t) =
+  match stream with
   | Empty -> acc
-  | Yield (x, k) -> fold f (f acc x) k
+  | Yield (x, k) -> fold f (f acc x) (k ())
 
-let rec map f stream () =
-  match stream () with
+let rec map f (stream : 'a t) =
+  match stream with
   | Empty -> Empty
-  | Yield (x, k) -> Yield (f x, map f k)
-
-let rec take n stream () =
-  match stream () with
-  | Empty -> Empty
-  | Yield (x, k) ->
-    if n = 0 then Empty
-    else Yield (x, (take (n - 1) k))
+  | Yield (x, k) -> Yield (f x, fun () -> map f (k ()))
 
 let rec head stream =
-  match stream () with
+  match stream with
   | Empty -> None
   | Yield (x, _) -> Some x
 
 let is_empty (stream : 'a t) =
-  match stream () with
+  match stream with
   | Empty -> true
-  | Yield _ -> false
+  | _ -> false
 
-let rec of_list l () =
-  match l with
+let rec of_list list : 'a t =
+  match list with
   | [] -> Empty
-  | x :: xs -> Yield (x, of_list xs)
+  | x :: xs -> Yield (x, fun () -> of_list xs)
 
-let to_list stream =
+let to_list (stream : 'a t) =
   let rec loop acc stream' =
-    match stream' () with
+    match stream' with
     | Empty -> acc
-    | Yield (x, k) -> loop (List.cons x acc) k in
+    | Yield (x, k) -> loop (List.cons x acc) (k ()) in
   List.rev (loop [] stream)
 
-let of_string str =
-  let rec loop i () =
+let of_string str : char t =
+  let rec loop i =
     try
-      Yield (String.get str i, loop (i + 1))
+      Yield (String.get str i, fun () -> loop (i + 1))
     with Invalid_argument _ ->
       Empty in
   loop 0
 
-let rec filter p stream () =
-  match stream () with
+let rec filter p stream  =
+  match stream with
   | Empty -> Empty
   | Yield (x, k) ->
     if p x then
-      Yield (x, filter p k)
+      Yield (x, fun () -> filter p (k ()))
     else
-      filter p k ()
+      filter p (k ())
 
 let reject p stream =
   filter (fun x -> not (p x)) stream
 
 let next stream =
-  match stream () with
+  match stream with
   | Empty -> None
-  | Yield (x, stream') -> Some (x, stream')
+  | Yield (x, stream') -> Some (x, stream' ())
 
